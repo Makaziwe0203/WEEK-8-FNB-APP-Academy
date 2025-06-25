62fc3cc6f08057702fa2037519a372a15aea874e
Makaziwe0203 <ntsenyanamakaziwe@gmail.com>
2025-06-25 03:57:02 +0200

    Lesson5: Dictionary party 2

---
+++ python.py
@@ -0,0 +1,21 @@
+#Operators
+
+#Addition (+)
+#Subtraction (-)
+#Multiplication (*)
+#Division (/)
+#Modulus (%)
+#Exponbts (**)
+
+x =10
+y =2
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+
+x=10
+x-=2
+print(x)
